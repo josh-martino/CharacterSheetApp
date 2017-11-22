0ec51379ae635318b079e00253323704ca566af8
Josh Martin <Josh.Martin@samtec.com>
2017-11-22 12:05:18 -0500

    minor improvements, still working on display

--- CharacterSheetApp/Controllers/HomeController.cs
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -12,12 +12,12 @@ namespace CharacterSheetApp.Controllers
         public ActionResult Index()
         {
             //return our GetAll method so that it will pass the entire list to be displayed.
-            return View(Models.Character.GetAll());
+            return View(Character.GetAll());
         }
 
         public ActionResult Create(string characterName)
         {
-            Models.Character.Create(characterName);
+            Character.Create(characterName);
             //instead of returning the view we'll redirect to the action in our home controller.
             //This keeps the user from creating a character twice by refreshing, avoiding duplicate code
             return RedirectToAction("Index");
