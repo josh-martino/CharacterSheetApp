ee6d34a886290ded86a904fe88fa44d55b79ff76
Josh Martin <Josh.Martin@samtec.com>
2017-11-22 09:03:32 -0500

    Pushing up project

---
+++ CharacterSheetApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CharacterSheetApp.Models;
+
+namespace CharacterSheetApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create(string characterName)
+        {
+            var model = new CharacterSheetApp.Models.Home();
+            model.Name = characterName;
+            return View("Index", model);
+        }
+    }
+}
